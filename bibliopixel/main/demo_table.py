7dda147b9b76dc680b82efbd6a5e3d37f6a83fc1
Oliver Keller <oli.keller@gmail.com>
2020-03-03 21:10:51 +0100

    add fluidicData demo
    run as 'bp demo fluidicData'
    TODO: find out how to achieve the same by loading an external python
    module via the bp CLI
    it's quite acceptable develop code as a bp demo if bibliopixel is
    installed in dev mode, see scripts/developer/install.sh

--- bibliopixel/main/demo_table.py
+++ bibliopixel/main/demo_table.py
@@ -6,6 +6,8 @@ from bibliopixel.util import class_name
 from BiblioPixelAnimations.circle.bloom import CircleBloom
 from BiblioPixelAnimations.circle.swirl import Swirl
 from BiblioPixelAnimations.circle.hyperspace import HyperspaceRainbow
+from BiblioPixelAnimations.strip.Wave import Wave
+import math
 
 
 BLOOM = {
@@ -182,10 +184,206 @@ def circle(args):
 
     return anim
 
+####################
+# adding fluidicData as demo since loading of python modules via bp CLI seems broken
+
+num_rings = []
+num_points = []
+
+num_pods = 8
+num_flowers = 6
+pod_leds = 8
+flower_leds = 12
+pod_ring = [0, 1, 2, 3, 4, 5, 6, 7] #    rings, steps = geometry.make_circle_coord_map(pixels_per=[pods_leds])
+flower_ring = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+x_off = 200
+y_off = 200
+
+for ring in range(0,num_pods*2,2):
+    # 2x 8 LED rings per pod
+    points = geometry.make_circle_coord_map_positions(
+        [pod_ring], origin=(x_off, y_off, ring*10), radii=[1 * math.pi]*pod_leds)
+    num_rings += [[r+pod_leds*ring for r in pod_ring]] # add with offset
+    num_points += points
+    # second pod ring
+    points = geometry.make_circle_coord_map_positions(
+        [pod_ring], origin=(x_off, y_off, ring*10+4), radii=[1 * math.pi]*pod_leds)
+    num_rings += [[r+pod_leds*(ring+1) for r in pod_ring]] # add with offset
+    num_points += points
+
+
+for ring in range(num_flowers):
+    # distribute as spiral
+    theta = ring / 3 * math.pi * 2
+    x = int(x_off + math.sin(theta) * 4)
+    y = int(y_off + math.cos(theta) * 4)
+    # 1x 12 LED ring per flower
+    points = geometry.make_circle_coord_map_positions(
+        [flower_ring], origin=(x, y, (num_pods-1)*23 + ring*7), radii=[3 * math.pi]*flower_leds)
+    num_rings += [[r+flower_leds*ring for r in flower_ring]] # add with offset
+    num_points += points
+
+
+
+FD_PROJECT = {
+    'driver': {
+        'typename': 'simpixel',
+        'num': len(num_points),
+        'pixel_positions': num_points,
+    },
+    'controls': {
+    'typename': '.rest',
+    'verbose': 'true',
+    },
+    'layout': {
+        'typename': 'circle',
+        'rings': num_rings,
+        'maxAngleDiff': 0,
+    },
+
+    'animation': {
+        'typename': 'split',
+        'size': [16,16,16,16,16,16,16,16,12,12,12,12,12,12],
+        'animations': [
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.Wave.Wave',
+                },
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.WhiteTwinkle.WhiteTwinkle',
+                        'speed': 20
+                },
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.strip.Searchlights.Searchlights',
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.LarsonScanners.LarsonScanner',
+                        'color': 'white'
+                },
+                'run': {
+                    'amt': 3,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.strip.Rainbows.Rainbow',
+                'run': {
+                    'amt': 5,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.LarsonScanners.LarsonScanner',
+                        'color': 'red'
+                },
+                'run': {
+                    'amt': 4,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.LarsonScanners.LarsonScanner',
+                        'color': 'green'
+                },
+                'run': {
+                    'amt': 7,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': {
+                    'typename': 'BiblioPixelAnimations.strip.Rainbows.RainbowCycle', 
+                 },
+                'run': {
+                    'amt': 9,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.LarsonScanners.LarsonScanner',
+                        'color': 'blue'
+                },
+                'run': {
+                    'amt': 2,
+                    'fps': 15,
+                },
+            },
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.Wave.Wave',
+                        'color': 'green'
+                },
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.strip.Rainbows.Rainbow',
+                'run': {
+                    'amt': 12,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.strip.ColorFade.ColorFade',
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': {
+                    'typename':
+                        'BiblioPixelAnimations.strip.Wave.Wave',
+                        'color': 'blue'
+                },
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.strip.Rainbows.Rainbow',
+                'run': {
+                    'amt': 15,
+                    'fps': 30,
+                },
+            }
+        ]
+    }
+}
 
 DEMO_TABLE = {
     'bloom': BLOOM,
     'circle': CIRCLE_PROJECT,
     'cube': CUBE_PROJECT,
     'matrix': MATRIX_PROJECT,
+    'fluidicData': FD_PROJECT
 }
